d1693515c0a12831905321086869706a286f06a8
elwinspranata <elwinpranata02@gmail.com>
2025-01-13 13:21:36 +0700

    Implementasi fungsi insert dan getAll mahasiswa di NetworkRepositoryMhs

---
+++ app/src/main/java/com/example/praktikum14/repository/NetworkRepositoryMhs.kt
@@ -0,0 +1,37 @@
+package com.example.praktikum14.repository
+
+import com.example.praktikum14.model.Mahasiswa
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
+
+class NetworkRepositoryMhs(
+    private val firestore: FirebaseFirestore
+) : RepositoryMhs {
+
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa: ${e.message}")
+        }
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
+        val mhsCollection = firestore.collection("Mahasiswa")
+            .orderBy("nim", Query.Direction.DESCENDING)
+            .addSnapshotListener { value, error ->
+                if (value != null) {
+                    val mhsList = value.documents.mapNotNull {
+                        it.toObject(Mahasiswa::class.java)!!
+                    }
+                    trySend(mhsList)
+                }
+            }
+        awaitClose {
+            mhsCollection.remove()
+        }
+    }
