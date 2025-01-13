b2d93b58a87e86b920ac44b49daf232ee51bdff9
elwinspranata <elwinpranata02@gmail.com>
2025-01-13 13:30:10 +0700

    Tambah fungsi getMhs di HomeViewModel untuk fetch data mahasiswa dan update UI state

--- app/src/main/java/com/example/praktikum14/ui/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum14/ui/home/viewmodel/HomeViewModel.kt
@@ -4,7 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum14.repository.RepositoryMhs
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
 
@@ -16,3 +20,23 @@ class HomeViewModel(
     init {
         getMhs()
     }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            repositoryMhs.getAllMhs()
+                .onStart {
+                    mhsUiState = HomeUiState.Loading
+                }
+                .catch {
+                    mhsUiState = HomeUiState.Error(e = it)
+                }
+                .collect {
+                    mhsUiState = if (it.isEmpty()) {
+                        HomeUiState.Error(Exception("Belum ada data Mahasiswa"))
+                    } else {
+                        HomeUiState.Success(it)
+                    }
+                }
+        }
+    }
+
